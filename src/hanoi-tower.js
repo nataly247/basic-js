333c76c40e7fc4e60a2a887421e2ff0dd3431863
Nataly <nataly247@gmail.com>
2021-03-30 00:14:24 +0300

    feat: fix hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,5 @@
-module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    let turns = Math.pow(2, disksNumber) - 1;
-    let seconds = turns / (turnsSpeed / 3600);
-    return {
-        turns,
-        seconds
-    };
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+    let turns = (2**disksNumber) - 1;
+    let seconds = turns/(turnsSpeed/3600);
+    return {seconds: seconds, turns: turns};
 }
