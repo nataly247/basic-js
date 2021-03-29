f47e98a21294e949cae3ffefe893c61726d2b97e
Nataly <nataly247@gmail.com>
2021-03-29 23:56:13 +0300

    feat: add extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    let {
+        repeatTimes,
+        separator,
+        addition,
+        additionRepeatTimes,
+        additionSeparator
+    } = options; // destruct
+
+    str = (str == null) ? 'null' : str.toString();
+    if (typeof addition === 'undefined') {
+        addition = '';
+    } else if (addition == null) {
+        addition = 'null';
+    } else {
+        addition = addition.toString();
+    }
+
+    let getStr = function (times, item, separator) {
+        arr = Array(times);
+        arr.fill(item);
+        strFromArr = arr.join(separator);
+        return strFromArr;
+    }
+    let checkStr = function (str, defaultVal) {
+        return (typeof separator === 'undefined') ? defaultVal : str;
+    }
+    let checkNum = function (num) {
+        return (typeof num === 'number' && isFinite(num)) ? num : 1;
+    }
+
+    separator = checkStr(separator, '+');
+    additionSeparator = checkStr(additionSeparator, '|');
+    repeatTimes = checkNum(repeatTimes);
+    additionRepeatTimes = checkNum(additionRepeatTimes);
+
+    additionalStr = getStr(additionRepeatTimes, addition, additionSeparator);
+    resStr = getStr(repeatTimes, str + additionalStr, separator);
+
+    return resStr;
 };
   
