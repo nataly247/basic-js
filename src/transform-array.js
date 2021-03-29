b632643ecf6a2c486da0d1516f7734c8057377de
Nataly <nataly247@gmail.com>
2021-03-29 23:59:52 +0300

    feat: add transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+    if (!(arr && typeof arr === 'object' && arr.constructor === Array)) {
+        throw Error;
+    }
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    if (arr.length == 0) {
+        return [];
+    }
+
+    let transformedArr = [];
+    const flags = [
+        '--discard-next',
+        '--discard-prev',
+        '--double-next',
+        '--double-prev',
+    ];
+
+    let missOne = false;
+    arr.forEach((cur, i, array) => {
+        if (flags.includes(cur)) {
+            switch (cur) {
+                case '--discard-next':
+                    missOne = true;
+                    break;
+                case '--discard-prev':
+                    transformedArr.pop();
+                    break;
+                case '--double-next':
+                    if (typeof array[i + 1] !== 'undefined') {
+                        transformedArr.push(array[i + 1]);
+                    }
+                    break;
+                default: // -- double-prev
+                    if (typeof array[i - 1] !== 'undefined') {
+                        transformedArr.push(array[i - 1]);
+                    }
+                    break;
+            }
+        } else {
+            if (!missOne) {
+                transformedArr.push(cur)
+            } else {
+                missOne = false;
+            }
+        }
+    });
+
+    return transformedArr;
 };
